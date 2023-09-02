1bff9d88953d42d2fa5ca2244bc6d61373b15939
RABrL <robin_brito@hotmail.com>
2023-09-01 19:34:54 -0500

    Move themeprovider to provider folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 

97c6ec682d88eac146a516cc7fe635161ea298a8
RABrL <robin_brito@hotmail.com>
2023-09-07 13:12:07 -0500

    Update dependencies

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -64,20 +64,20 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
               <FormControl>
                 <div className="relative p-4 pb-6">
                   <button
-                    type="button"
                     onClick={() => onOpen('messageFile', { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
                   </button>
                   <Input
+                    type="text"
+                    {...field}
                     disabled={isLoading}
                     autoComplete="off"
                     placeholder={`Message ${
                       type === 'conversation' ? name : `#${name}`
                     }`}
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
-                    {...field}
                   />
                   <div className="absolute top-7 right-8">
                     <EmojiPicker

36cda8b0b207e7abc63616b7293175659e697aab
RABrL <robin_brito@hotmail.com>
2023-09-08 11:42:41 -0500

    Improve the accesibility buttons in input component

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -140,6 +140,7 @@ export const ChatInput = ({
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
+                    <span className="sr-only">Add file</span>
                   </button>
                   <Input
                     disabled={isLoading}

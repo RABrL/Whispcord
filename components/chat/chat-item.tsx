266e6fd427767b42b7c189976cb038b702119448
RABrL <robin_brito@hotmail.com>
2023-09-07 19:06:13 -0500

    add autofocus and autocomplete off in the input on edit functionality

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -204,6 +204,8 @@ export const ChatItem = ({
                     <FormItem className="flex-1">
                       <FormControl>
                         <Input
+                          autoFocus
+                          autoComplete='off'
                           disabled={isLoading}
                           className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                           placeholder="Edited message"

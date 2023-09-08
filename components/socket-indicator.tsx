e19c0051948f73d30bd0bd04aad8aefa01c19d43
RABrL <robin_brito@hotmail.com>
2023-09-07 19:26:23 -0500

    Improve the responsive of socket indicator

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -6,17 +6,23 @@ import { Badge } from '@/components/ui/badge'
 export const SocketIndicator = () => {
   const { isConnected } = useSocket()
 
-  if(!isConnected) {
+  if (!isConnected) {
     return (
-      <Badge variant='outline' className='bg-yellow-600 text-white border-none'>
-        Fallback: Polling every 1s
+      <Badge
+        variant="outline"
+        className="bg-yellow-600 text-white border-none h-4 w-4 p-0 md:px-2.5 md:py-0.5 md:w-full md:h-full"
+      >
+        <span className="hidden md:flex">Fallback: Polling every 1s</span>
       </Badge>
     )
   }
 
   return (
-    <Badge variant='outline' className='bg-emerald-600 text-white border-none'>
-      Live: Real-time updates
+    <Badge
+      variant="outline"
+      className="bg-emerald-600 text-white border-none h-4 w-4 p-0 md:px-2.5 md:py-0.5 md:w-full md:h-full"
+    >
+      <span className="hidden md:block">Live: Real-time updates</span>
     </Badge>
   )
 }

acfef8c2aa22de87e334cd669bb6f8efa39d0899
RABrL <robin_brito@hotmail.com>
2023-09-12 11:46:03 -0500

    Improve the schema for create channel verification

--- schemas/create-channel.ts
+++ schemas/create-channel.ts
@@ -5,7 +5,7 @@ export const channelSchema = z.object({
   name: z
     .string()
     .min(1, { message: 'Server name is required' })
-    .refine((name) => name !== 'general', {
+    .refine((name) => name.trim() !== 'general', {
       message: "Channel name cannot be 'general'"
     }),
   type: z.nativeEnum(ChannelType)
